f6b97aff5abe91676b9402915b034b30082c25c9
Robert Dabu <robert.dabu@gmail.com>
2017-04-12 00:26:19 -0400

    add comments

--- overloading.php
+++ overloading.php
@@ -15,10 +15,12 @@
         
         public function __set($name, $value)
         {
+            echo "Setting '$name' to '$value'<br/>";
             $this->str[$name] = $value;
         }
         public function __get($name)
         {
+            echo "Getting '$name'<br/>";
             return $this->str[$name];
         }
         
@@ -76,7 +78,6 @@
     $obj->overloaded_property = "Using __set method";
     echo $obj->overloaded_property; // uses __get method
     echo "<br/><br/>";
-    
     $obj->overloadedFunction("parameter1");
     echo "<br/><br/>";
     $obj->overloadedFunction("parameter1" , "parameter2");
@@ -87,4 +88,23 @@
     unset($obj->overloaded_property); //unset will invoke __unset method
     isset($obj->overloaded_property);
     
+    /*
+    Coming from the constructor
+
+    Setting 'overloaded_property' to 'Using __set method'
+    Getting 'overloaded_property'
+    Using __set method
+    
+    string(18) "overloadedFunction" 
+    array(1) { [0]=> string(10) "parameter1" } 
+    
+    string(18) "overloadedFunction" 
+    array(2) { [0]=> string(10) "parameter1" [1]=> string(10) "parameter2" } 
+    
+    Magic method invoked while method overloading with static access
+
+    Property $overloaded_property is set.
+    $overloaded_property is unset 
+    Property $overloaded_property is not set.
+    */
 ?>
